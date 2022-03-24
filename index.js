93560952d2cc8efe19562c8986b5db8dd22637bc
Ellie Diaz <elliediaz@Ellies-Air.lan>
2022-03-24 18:41:27 -0500

    updated code

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-//Original Images
-function newImg (imgLink, left, bottom) {
+
+function newImg(imgSrc, left, bottom) {
     let newImg = document.createElement('img')
-    newImg.src = imgLink
+    newImg.src = imgSrc
     newImg.style.position = 'fixed'
     newImg.style.left = left + 'px'
     newImg.style.bottom = bottom +'px'
     document.body.append(newImg)
+    return newImg
 }
+
+function newItem(imgSrc, left, bottom) {
+    let newItem = newImg(imgSrc, left, bottom)
+
+    newItem.addEventListener('dblclick', function(){
+    newItem.remove()
+    })
+}
+//Images
 newImg('assets/green-character.gif', 100, 100)
 newImg('assets/pine-tree.png', 450, 200)
 newImg('assets/tree.png', 200, 300)
@@ -15,30 +25,8 @@ newImg('assets/crate.png', 150, 200)
 newImg('assets/well.png', 500, 425)
 
 //Dissapearing Items
-function newItem (imgSrc, left1, bottom1) {
-let newItem = document.createElement('img')
-newItem.src = imgSrc
-newItem.style.position = 'fixed'
-newItem.style.left = left1 +'px'
-newItem.style.bottom = bottom1+'px'
-document.body.append(newItem)
-
-newItem.addEventListener('dblclick', function(){
-    newItem.remove()
-})
-}
-
 newItem('assets/sword.png', 500,405)
 newItem('assets/sheild.png', 165,185)
 newItem('assets/staff.png', 600,100)
 
-//Background
-function setBackground (src, width, height, alt) {
-    let setBackground = document.createElement("img");
-    setBackground.src = src;
-    setBackground.width = width;
-    setBackground.height = height;
-    setBackground.alt = alt; 
-    document.body.appendChild(setBackground);
-}
-setBackground ('assets/grass.png', window.innerWidth, window.innerHeight, grass)
+
